7f343d944bb3456c982f5126c707e31ba8290ff4
ozboggy <50730773+ozboggy@users.noreply.github.com>
2025-06-16 12:12:11 +1000

    Add files via upload

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+USERNAME = os.getenv('OPENSKY_USERNAME')
+PASSWORD = os.getenv('OPENSKY_PASSWORD')
+
+import requests
+url = "https://opensky-network.org/api/states/all"
+r = requests.get(url, auth=(USERNAME, PASSWORD))
+data = r.json()
+print(data)
 
 import streamlit as st
 import requests
